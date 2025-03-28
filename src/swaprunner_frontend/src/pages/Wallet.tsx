da73c4b38be8106ee12622d13a8d1597aacdc22b
snassy-icp <snassy.icp@gmail.com>
2025-03-28 04:40:18 +0100

    wallet subaccounts fe

--- src/swaprunner_frontend/src/pages/Wallet.tsx
+++ src/swaprunner_frontend/src/pages/Wallet.tsx
@@ -14,7 +14,7 @@ import '../styles/Wallet.css';
 import { AddSubaccountModal } from '../components/AddSubaccountModal';
 import '../styles/AddSubaccountModal.css';
 import { Principal } from '@dfinity/principal';
-import { bytesToHex, formatBytes, formatPrincipal } from '../utils/subaccounts';
+import { formatHex, formatBytes, formatPrincipal } from '../utils/subaccounts';
 
 interface NamedSubaccount {
   name: string;
@@ -782,6 +782,8 @@ export const WalletPage: React.FC = () => {
                           <div className="subaccounts-list">
                             {token.subaccounts.map((subaccount) => {
                               const isExpanded = expandedSubaccounts.has(`${token.canisterId}-${subaccount.name}`);
+                              console.log(subaccount);
+                              console.log(formatHex(subaccount.subaccount));
                               return (
                                 <div key={subaccount.name} className={`subaccount-item ${isExpanded ? 'expanded' : ''}`}>
                                   <div className="subaccount-header"
@@ -838,7 +840,7 @@ export const WalletPage: React.FC = () => {
                                       </div>
                                       <div className="subaccount-format">
                                         <div className="format-label">Hex:</div>
-                                        <code>0x{bytesToHex(subaccount.subaccount)}</code>
+                                        <code>0x{formatHex(subaccount.subaccount)}</code>
                                       </div>
                                       <div className="subaccount-format">
                                         <div className="format-label">Bytes:</div>
