6eaddf5a0646e27e3d597d00ea221107bca9f8c9
snassy-icp <snassy.icp@gmail.com>
2025-04-07 11:41:46 +0200

    /sponsors

--- src/swaprunner_frontend/src/pages/Sponsors.tsx
+++ src/swaprunner_frontend/src/pages/Sponsors.tsx
@@ -4,7 +4,7 @@ import '../styles/Sponsors.css';
 export const Sponsors: React.FC = () => {
     return (
         <div className="sponsors-page">
-            <div className="swap-box">
+            <div className="sponsors-box">
                 <div className="sponsors-content">
                     <h1>Sponsors</h1>
                     <div className="coming-soon">
